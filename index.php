ed4e8d58fe79e6f5f63aa0daa0d4ce9ddbaaf70e
Rokas Reizgys <rokas.student@gmail.com>
2024-01-05 16:10:30 +0200

    added style to the form

--- index.php
+++ index.php
@@ -10,11 +10,25 @@
 		</p>
 
 		<!-- Form for entering credentials -->
-		<form method="post" action="saveCredentials.php">
-			Project ID: <input type="text" name="project_id" onchange="saveCredentials()"><br>
-			Private Key: <textarea name="private_key" onchange="saveCredentials()"></textarea><br>
-			<input type="submit" value="Save">
+		<form method="post" action="saveCredentials.php" class="form-container">
+			<div class="form-row">
+				<label class="form-label" for="project_id">Project ID:</label>
+				<div class="form-input">
+					<input type="text" id="project_id" name="project_id" onchange="saveCredentials()">
+				</div>
+			</div>
+			<div class="form-row">
+				<label class="form-label" for="private_key">Private Key:</label>
+				<div class="form-textarea">
+					<textarea id="private_key" name="private_key" onchange="saveCredentials()"></textarea>
+				</div>
+			</div>
+			<div class="form-row">
+				<div class="form-label"></div> <!-- Empty div for alignment -->
+				<input class = "save_button" type="submit" value="Save">
+			</div>
 		</form>
+
 
 		<ol>
 			<li><a href="direct.php">Create merchant order (direct API result usage)</a></li>
